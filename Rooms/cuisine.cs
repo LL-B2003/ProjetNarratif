edcfa78f86613566a7be381b9d1bb815acb152ea
Loic <loic.le.bergeron@outlook.com>
2023-11-10 15:29:21 -0500

    cuisine room ajout

--- Rooms/cuisine.cs
+++ Rooms/cuisine.cs
@@ -4,17 +4,19 @@
     {
         internal override string CreateDescription() =>
             @"Tu entre dans la [cuisine]. Tu constates plusieurs choses. Il y a un comptoir où vous vous installez.
-            À votre droite se trouve une [armoire] de pâte ";
+            À votre droite se trouve une [armoire]. Il y a un [frigo] et [bocal à olive]. Il y a deux pâtes à pizza";
 
             internal override void ReceiveChoice(string choice)
         {
             switch (choice)
             {
                 case "cuisine":
-                    
+                    Console.WriteLine();
                     break;
                 case "armoire":
-                    
+                    Console.WriteLine();
+                    break;
+                case "":
                     break;
                 default:
                     Console.WriteLine("Commande invalide.");
